9d91d5190e6b06a3a0f136ecb3dcac695dfa26e5
zach-c-erickson <zce1382@g.rit.edu>
2022-02-25 17:06:55 -0500

    Updated Shop

--- Game4/Assets/Scripts/ShopController.cs
+++ Game4/Assets/Scripts/ShopController.cs
@@ -14,8 +14,18 @@ public class ShopController : MonoBehaviour
 
     public Image upgradeBar1;
 
-    public GameObject notAvailableButton1;
-    public GameObject availableButton1;
+    
+    public Button button1;
+    public Text buttonText1;
+    public GameObject progressSlider1;
+    public Timer progressTimer1;
+    public GameObject upgradeCompleteButton;
+    public GameObject speedUpButton;
+    public GameObject GoldPurchaseCanvas;
+    public GameObject ItemPurchaseCanvas;
+    public GameObject UpgradeCanvas;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -27,22 +37,44 @@ public class ShopController : MonoBehaviour
     {
         if(resourceManager.GetGoldAmount() < upgrade1.cost)
         {
-            availableButton1.gameObject.SetActive(false);
-            notAvailableButton1.gameObject.SetActive(true);
+            button1.interactable = false;
+            buttonText1.text = "Not AvAilAble";
         }
 
         else
         {
-            availableButton1.gameObject.SetActive(true);
-            notAvailableButton1.gameObject.SetActive(false);
+            button1.interactable = true;
+            buttonText1.text = "Buy UpgrAde";
+            
         }
     }
 
-    public void Upgrade()
+    public void UpgradeStart()
+    {
+        resourceManager.RemoveGold(upgrade1.cost);
+        button1.gameObject.SetActive(false);
+        
+        progressSlider1.gameObject.SetActive(true);
+        speedUpButton.gameObject.SetActive(true);
+
+
+        progressTimer1.StartTimer(30f);
+        
+        
+    }
+
+    public void UpgradeComplete()
+    {
+        upgradeCompleteButton.SetActive(true);
+        progressSlider1.gameObject.SetActive(false);
+        speedUpButton.gameObject.SetActive(false);
+        progressTimer1.Reset();
+    }
+
+    public void PurchaseUpgrade()
     {
         
-
-        for(int i = 0; i < upgradeProgressSprites.Count; i++)
+        for (int i = 0; i < upgradeProgressSprites.Count; i++)
         {
             if(upgradeBar1.sprite == upgradeProgressSprites[i])
             {
@@ -51,6 +83,48 @@ public class ShopController : MonoBehaviour
             }
         }
 
-        resourceManager.RemoveGold(upgrade1.cost);
+        upgradeCompleteButton.SetActive(false);
+        button1.gameObject.SetActive(true);
+
     }
+
+    public void SpeedUp()
+    {
+        if(resourceManager.GetPremiumCurrencyAmount() >= 2)
+        {
+            UpgradeComplete();
+            resourceManager.RemovePremiumCurrency(2);
+        }
+
+        else
+        {
+
+            GoldPurchaseCanvas.gameObject.SetActive(true);
+        }
+
+
+    }
+
+    public void UpgradeTab()
+    {
+        GoldPurchaseCanvas.gameObject.SetActive(false);
+        ItemPurchaseCanvas.gameObject.SetActive(false);
+        UpgradeCanvas.gameObject.SetActive(true);
+    }
+
+    public void ItemTab()
+    {
+        GoldPurchaseCanvas.gameObject.SetActive(false);
+        ItemPurchaseCanvas.gameObject.SetActive(true);
+        UpgradeCanvas.gameObject.SetActive(false);
+    }
+
+    public void GoldTab()
+    {
+        GoldPurchaseCanvas.gameObject.SetActive(true);
+        ItemPurchaseCanvas.gameObject.SetActive(false);
+        UpgradeCanvas.gameObject.SetActive(false);
+    }
+
+
 }
